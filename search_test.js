9f33971048d4a3ccede7d3732d2fb9c422aec16b
b-kuhu <Kuhu Bhatnagar>
2022-09-23 18:04:54 +0530

    initial commit

---
+++ search_test.js
@@ -0,0 +1,16 @@
+Feature('search');
+
+Scenario('search for chocolate cake', async({ I }) => {
+  I.amOnPage('https://www.fnp.com');
+  I.click('Select Delivery Location','.jss7');
+  I.click('Select Delivery Location','.jss7');
+  I.fillField('Enter Pincode','201001');
+  I.click('//ul[@class="location-popup_menu__IUQjd"]/li[1]');
+  I.click('(//button[@type="button"])[8]'); 
+  I.fillField('Search flowers, cakes, gifts, etc.','Chocolate Cake');
+  I.pressKey('Enter');
+  I.waitForNavigation();
+  const url = await I.grabCurrentUrl();
+  I.seeCurrentUrlEquals(url);
+  I.see('Chocolate Cake');
+});
