735b55347790a41612f2e40ff5ba3f5caa675ce9
b-kuhu <Kuhu Bhatnagar>
2022-09-26 12:30:17 +0530

    check search result

--- search_test.js
+++ search_test.js
@@ -5,13 +5,13 @@ Scenario('search for chocolate cake', async({ I }) => {
   I.click('Select Delivery Location','.jss7');
   I.click('Select Delivery Location','.jss7');
   I.fillField('Enter Pincode','201001');
-  I.click('//ul[@class="location-popup_menu__IUQjd"]/li[1]');
+  I.click('/ul/li/div');
   I.click('(//button[@type="button"])[8]'); 
   I.fillField('Search flowers, cakes, gifts, etc.','Chocolate Cake');
   I.pressKey('Enter');
   I.waitForNavigation();
   const url = await I.grabCurrentUrl();
   I.seeCurrentUrlEquals(url);
-  const result = await I.grabTextFrom('//h1[@itemprop="name"]');
-  result.match('Chocolate Cake');
+  const result = await I.grabTextFrom('//div/a[@title]');
+  console.log(result.match(["Chocolate"]));
 });
