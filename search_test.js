f0f776ee0e9126a11d286867f9cf9e6479d49c12
b-kuhu <Kuhu Bhatnagar>
2022-09-27 14:14:13 +0530

    narrowed search results

--- search_test.js
+++ search_test.js
@@ -5,13 +5,15 @@ Scenario('search for chocolate cake', async({ I }) => {
   I.click('Select Delivery Location','.jss7');
   I.click('Select Delivery Location','.jss7');
   I.fillField('Enter Pincode','201001');
-  I.click('/ul/li/div');
-  I.click('(//button[@type="button"])[8]'); 
+  I.click('(//ul/li/div)[1]');
+  I.click('(//button[@type="button"])[6]'); 
   I.fillField('Search flowers, cakes, gifts, etc.','Chocolate Cake');
   I.pressKey('Enter');
   I.waitForNavigation();
   const url = await I.grabCurrentUrl();
   I.seeCurrentUrlEquals(url);
-  const result = await I.grabTextFrom('//div/a[@title]');
-  console.log(result.match(["Chocolate"]));
+  const result = await I.grabTextFrom('(//a[@title])[3]');
+  console.log(result.match('Chocolate Cake'));
+  I.see('Chocolate Cake','(//a[@title])[3]');
+  
 });
